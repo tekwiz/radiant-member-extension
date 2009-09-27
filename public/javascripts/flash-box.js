9c78b0e603b1bde172af7ec7420026b1a7a734f8
Travis D. Warlick, Jr <warlickt@operissystems.com>
2009-09-27 15:38:37 -0500

    change to jQuery

---
+++ public/javascripts/flash-box.js
@@ -0,0 +1,20 @@
+jQuery.fn.flashBox = function( options ) {
+  var opts = $.extend(jQuery.fn.flashBox.defaults, options);
+  var flash_raw = $.cookies.get(opts.cookieName);
+  if(flash_raw == null) { return this; }
+  var flash = $.evalJSON(unescape(flash_raw.replace(/\+/g, ' ')));
+  if((txt = flash.notice) != 'undefined') {
+    $(this).text(txt);
+    $(this).show();
+    $(this).addClass(opts.flashNoticeClass);
+  } else if((txt = flash.error) != 'undefined') {
+    $(this).text(txt);
+    $(this).show():
+    $(this).addClass(opts.flashErrorClass);
+  }
+  $.cookies.del(opts.cookieName, {path: '/'})
+  return this;
+}
+jQuery.fn.flashBox.defaults = { noticeClass : 'flash-notice',
+                                errorClass  : 'flash-error',
+                                cookieName  : 'flash' };
