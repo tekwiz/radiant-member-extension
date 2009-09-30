5c6484f740cfa90b8f5951653b873cc0b10663d9
Eli Van Zoeren <design@elivz.com>
2009-09-29 22:37:07 -0400

    Fix flash-box.js typos

--- public/javascripts/flash-box.js
+++ public/javascripts/flash-box.js
@@ -9,7 +9,7 @@ jQuery.fn.flashBox = function( options ) {
     $(this).addClass(opts.flashNoticeClass);
   } else if((txt = flash.error) != 'undefined') {
     $(this).text(txt);
-    $(this).show():
+    $(this).show();
     $(this).addClass(opts.flashErrorClass);
   }
   $.cookies.del(opts.cookieName, {path: '/'})
@@ -18,4 +18,7 @@ jQuery.fn.flashBox = function( options ) {
 jQuery.fn.flashBox.defaults = { noticeClass : 'flash-notice',
                                 errorClass  : 'flash-error',
                                 cookieName  : 'flash' };
-$('.flash-box').flashBox();
+$(document).ready(function() {
+	$('.flash-box').flashBox();
+});
+
